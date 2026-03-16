13c23c111397e4ebadbd9a0f59146ab712624887
bot4pk <clawbot4pk@gmail.com>
2026-03-16 23:23:49 +0530

    feat(core): add router logging and pass clippy-deny checks

--- src/server.rs
+++ src/server.rs
@@ -1,50 +1,50 @@
-use std::net::TcpListener;
-use std::convert::TryFrom;
 use status_code::StatusCode;
+use std::convert::TryFrom;
+use std::net::TcpListener;
 
-use crate::http::{Request, request,Response, status_code, response,parse_error};
-use std::io::{Read,Write};
+use crate::http::{parse_error, request, response, status_code, Request, Response};
+use std::io::{Read, Write};
 
 pub trait Handler {
-    fn handle_request(&mut self,request:&Request)->Response;
-    fn handle_bad_reques(&mut self,e:&parse_error)->Response{
-        println!("There is error in parsing request {}",e);
+    fn handle_request(&mut self, request: &Request) -> Response;
+    fn handle_bad_reques(&mut self, e: &parse_error) -> Response {
+        println!("There is error in parsing request {}", e);
         Response::new(StatusCode::BadRequest, None)
     }
-    
 }
-pub struct Server{
-    addr:String,
+pub struct Server {
+    addr: String,
 }
-impl Server{
-    pub fn new (addr:String)->Self{
+impl Server {
+    pub fn new(addr: String) -> Self {
         Self { addr }
     }
-    pub fn run(self,mut handler:impl Handler){
-        let listner=TcpListener::bind(&self.addr).unwrap();
-        println!("Listening on address: {}",self.addr);
-        loop{
-           match listner.accept(){
-                Ok((mut stream,_))=>{
-                    let mut buf=[0;1024];
-                    match stream.read(&mut buf){
-                        Ok(_)=>{
-                            println!("Received a request: {}",String::from_utf8_lossy(&buf));
-                            let response=match Request::try_from(&buf[..]){
-                                Ok(request)=>handler.handle_request(&request), 
-                                Err(e)=>handler.handle_bad_reques(&e),
+    pub fn run(self, mut handler: impl Handler) {
+        let listner = TcpListener::bind(&self.addr).unwrap();
+        println!("Listening on address: {}", self.addr);
+        loop {
+            match listner.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buf = [0; 1024];
+                    match stream.read(&mut buf) {
+                        Ok(bytes_read) => {
+                            println!(
+                                "Received a request: {}",
+                                String::from_utf8_lossy(&buf[..bytes_read])
+                            );
+                            let response = match Request::try_from(&buf[..bytes_read]) {
+                                Ok(request) => handler.handle_request(&request),
+                                Err(e) => handler.handle_bad_reques(&e),
                             };
-                            if let Err(e)=response.send(&mut stream){
-                                println!("Failed to send response {}",e);
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response {}", e);
                             }
-                        },
-                        Err(e)=>println!("Error in reading buffer {}",e),
+                        }
+                        Err(e) => println!("Error in reading buffer {}", e),
                     };
-
                 }
-                Err(e)=>println!("Failed to establish a connection! {}",e),
+                Err(e) => println!("Failed to establish a connection! {}", e),
             };
-
         }
     }
 }
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,42 +1,100 @@
 use crate::http::Method;
 
+use super::http::{Request, Response, StatusCode};
 use super::server::Handler;
-use super::http::{Request,Response,StatusCode};
 use std::fs;
-pub struct WebsiteHandler{
-    public_path:String,
+
+pub struct WebsiteHandler {
+    public_path: String,
 }
-impl WebsiteHandler{
-    pub fn new(public_path:String)->Self{
-        Self{public_path}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
     }
-    pub fn read_file(&self,file_path:&str)->Option<String>{
-        let path=format!("{}/{}",self.public_path,file_path);
-        match fs::canonicalize(path){
-            Ok(path)=>{
-                if path.starts_with(&self.public_path){
+
+    pub fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
                     fs::read_to_string(path).ok()
-                }else{
-                    println!("Some one tried Directory traversal Attack!!!!!\n The path traversed is {}",file_path);
+                } else {
+                    println!(
+                        "Some one tried Directory traversal Attack!!!!!\n The path traversed is {file_path}"
+                    );
                     None
                 }
-            },
-            Err(_)=>None,
-        } 
+            }
+            Err(_) => None,
+        }
+    }
+
+    fn method_name(method: &Method) -> &'static str {
+        match method {
+            Method::GET => "GET",
+            Method::DELETE => "DELETE",
+            Method::POST => "POST",
+            Method::PUT => "PUT",
+            Method::HEAD => "HEAD",
+            Method::CONNECT => "CONNECT",
+            Method::OPTIONS => "OPTIONS",
+            Method::TRACE => "TRACE",
+            Method::PATCH => "PATCH",
+        }
+    }
+
+    fn respond_with_log(
+        &self,
+        method: &Method,
+        path: &str,
+        status_code: StatusCode,
+        body: Option<String>,
+    ) -> Response {
+        println!(
+            "[router] {} {} -> {} {}",
+            Self::method_name(method),
+            path,
+            status_code,
+            status_code.reason_phrase()
+        );
+        Response::new(status_code, body)
     }
 }
-impl Handler for WebsiteHandler{
-    fn handle_request(&mut self,request:&Request)->Response {
-        match request.method(){
-            Method::GET=>match request.path(){
-                "/"=>Response::new(StatusCode::Ok,self.read_file("index.html")),
-                "/hello"=>Response::new(StatusCode::Ok,Some("<h1>hellow there!!!</h1>".to_string())),
-                path=> match self.read_file(path){
-                    Some(contents)=>Response::new(StatusCode::Ok, Some(contents)),
-                    None=>Response::new(StatusCode::NotFound,None),
+
+impl Handler for WebsiteHandler {
+    fn handle_request(&mut self, request: &Request) -> Response {
+        let method = request.method();
+        let request_path = request.path();
+
+        match method {
+            Method::GET => match request_path {
+                "/" => {
+                    let body = self.read_file("index.html");
+                    let status_code = if body.is_some() {
+                        StatusCode::Ok
+                    } else {
+                        StatusCode::NotFound
+                    };
+                    self.respond_with_log(method, request_path, status_code, body)
+                }
+                "/hello" => self.respond_with_log(
+                    method,
+                    request_path,
+                    StatusCode::Ok,
+                    Some("<h1>hellow there!!!</h1>".to_string()),
+                ),
+                path => {
+                    let body = self.read_file(path);
+                    let status_code = if body.is_some() {
+                        StatusCode::Ok
+                    } else {
+                        StatusCode::NotFound
+                    };
+                    self.respond_with_log(method, request_path, status_code, body)
                 }
             },
-            _=> Response::new(StatusCode::NotFound,None),
+            _ => self.respond_with_log(method, request_path, StatusCode::NotFound, None),
         }
     }
 }
